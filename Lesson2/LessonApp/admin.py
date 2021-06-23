2ecc47e6f3296444a20630c47e05e3ca13612d4b
jayCde <68590879+jayCde@users.noreply.github.com>
2021-06-23 23:41:25 +0000

    Auth using Django's Abstract User Model - PostreSQL connection

---
+++ Lesson2/LessonApp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import CustomUser
+
+# Register your models here.
+class CustomUserAdmin(admin.ModelAdmin) :
+    list_display = ("first_name", "last_name", "username", "email", "password", 
+                    "location")
+
+admin.site.register(CustomUser, CustomUserAdmin)
